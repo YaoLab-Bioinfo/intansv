18f2e9963c9aa1e535bf8ae424403578b0959688
Wen Yao <ywhzau@gmail.com>
2015-01-25 07:24:32 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 1 commit.
    Commit information:
    
    Commit id: d7b2bf709808f2675cd874c0537f4e6888d20cc1
    
        add support for DELLY v0.6.1
    
    Committed by: andrewhzau
    Author Name: andrewhzau
    Commit date: 2015-01-25 15:14:26 +0800
    Author date: 2015-01-25 15:14:26 +0800
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/intansv@98665 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/readDelly.R
+++ R/readDelly.R
@@ -4,17 +4,17 @@ dellyCluster <- function(df)
 {
     maxReadPairSupp <- max(df$rp_support)
     dfFil <- df[df$rp_support>=(maxReadPairSupp/2), ]
-    dfFilIrange <- IRanges(start=dfFil$rp_left, end=dfFil$rp_right)
+    dfFilIrange <- IRanges(start=dfFil$start, end=dfFil$end)
     outTmp <- findOverlaps(dfFilIrange, reduce(dfFilIrange))
     dfFil$cludelly <- subjectHits(outTmp)
     dfFilRes <- ddply(dfFil, ("cludelly"), function(x){
         if(nrow(x)==1){
             return(x)
         } else {
-            LeftMin <- min(x$rp_left)
-            RightMax <- max(x$rp_right)
+            LeftMin <- min(x$start)
+            RightMax <- max(x$end)
             RangeLength <- RightMax-LeftMin
-            x$op <- (x$rp_right-x$rp_left)/RangeLength
+            x$op <- (x$end - x$start)/RangeLength
             if(any(x$op<0.8)){
                 return(NULL)
             } else {
@@ -27,283 +27,88 @@ dellyCluster <- function(df)
 ## Reading in the predicted SVs given by DELLY
 readDelly <- function(dataDir=".", regSizeLowerCutoff=100, 
                       regSizeUpperCutoff=1000000, readsSupport=3,
-                      method="DELLY") 
+                      method="DELLY", pass=TRUE, minMappingQuality=30) 
 {
-    ## reading deletions
-    dellyDelRpList <- list.files(dataDir, full.names=T, pattern=".+\\.del$")
-    dellyDelSrList <- list.files(dataDir, full.names=T, 
-                                 pattern=".+\\.del\\.br$")
+    ## reading in SV predictions
+    dellyFileList <- list.files(dataDir, full.names=T)
 
-    dellyDelRp <- lapply(dellyDelRpList, function(x) {
-        dellyData <- try(read.table(x, fill=T, as.is=T, comment.char="+"), silent=T)
+    dellyFileCont <- lapply(dellyFileList, function(x) {
+        dellyData <- try(read.table(x, as.is=T, head=F), silent=T)
         if (is.data.frame(dellyData)) {
-            dellyData <- dellyData[grepl("Deletion", dellyData$V7), ]
-            dellyData <- dellyData[, 1:7]
-            names(dellyData) <- c("chr", "rp_left", "rp_right", "rp_size", 
-                              "rp_support", "rp_map_quality", "id")
+            dellyData <- dellyData[, c(1:2, 7:8, 10)]
+            names(dellyData) <- c("chr", "start", "pass", 
+                              "info", "detail")
             return(dellyData)
         } else {
             return(NULL)
         }
     })
 
-    dellyDelSr <- lapply(dellyDelSrList, function(x) {
-        dellyData <- try(read.table(x, fill=T, col.names=paste("V", 1:13, sep=""), 
-                                as.is=T), silent=T)
-        if (is.data.frame(dellyData)) {
-            dellyData <- dellyData[grepl("Deletion", dellyData$V7), ]
-            dellyData <- dellyData[, 1:7]
-            names(dellyData) <- c("chr", "sr_left", "sr_right", "sr_size", 
-                              "sr_support", "sr_aln_quality", "id")
-            return(dellyData)
-        } else {
-            return(NULL)
-        }
-    })
-
-    dellyDelRpDf <- do.call(rbind, dellyDelRp)
-    dellyDelSrDf <- do.call(rbind, dellyDelSr)
-
-    dellyDelDf <- NULL
-    if (is.data.frame(dellyDelRpDf) & is.data.frame(dellyDelSrDf)) {
-        dellyDelDf <- merge(dellyDelRpDf, dellyDelSrDf, by=c("id", "chr"), all=T)
-        dellyDelDf$rp_left <- as.numeric(dellyDelDf$rp_left)
-        dellyDelDf$rp_right <- as.numeric(dellyDelDf$rp_right)
-        dellyDelDf$rp_size <- as.numeric(dellyDelDf$rp_size)
-        dellyDelDf <- dellyDelDf[dellyDelDf$rp_size>=regSizeLowerCutoff &
-                             dellyDelDf$rp_size<=regSizeUpperCutoff &
-                             dellyDelDf$rp_support>=readsSupport, ]
-        dellyDelDf$name <- NULL
-        dellyDelSrNna <- which(!(is.na(dellyDelDf$sr_size)))
-        if (length(dellyDelSrNna)>0) {
-            dellyDelDf[dellyDelSrNna, ]$rp_left <- dellyDelDf[dellyDelSrNna, ]$sr_left
-            dellyDelDf[dellyDelSrNna, ]$rp_right <- dellyDelDf[dellyDelSrNna, ]$sr_right
-            dellyDelDf[dellyDelSrNna, ]$rp_size <- dellyDelDf[dellyDelSrNna, ]$sr_size
-        }
-        dellyDelDf <- dellyDelDf[, 1:6]
-        dellyDelDf$rp_left <- as.numeric(dellyDelDf$rp_left)
-        dellyDelDf$rp_right <- as.numeric(dellyDelDf$rp_right)
-        dellyDelDf$rp_size <- as.numeric(dellyDelDf$rp_size)
-    } else if (is.data.frame(dellyDelRpDf)) {
-        dellyDelDf <- dellyDelRpDf
-        dellyDelDf$rp_left <- as.numeric(dellyDelDf$rp_left)
-        dellyDelDf$rp_right <- as.numeric(dellyDelDf$rp_right)
-        dellyDelDf$rp_size <- as.numeric(dellyDelDf$rp_size)
-        dellyDelDf <- dellyDelDf[dellyDelDf$rp_size>=regSizeLowerCutoff &
-                             dellyDelDf$rp_size<=regSizeUpperCutoff &
-                             dellyDelDf$rp_support>=readsSupport, ]
-        dellyDelDf$name <- NULL
-        dellyDelDf <- dellyDelDf[, 1:6]
-    } else if (is.data.frame(dellyDelSrDf)) {
-        dellyDelDf <- dellyDelSrDf
-        names(dellyDelDf)[2:5] <- c("rp_left","rp_right","rp_size","rp_support")
-        dellyDelDf$rp_left <- as.numeric(dellyDelDf$rp_left)
-        dellyDelDf$rp_right <- as.numeric(dellyDelDf$rp_right)
-        dellyDelDf$rp_size <- as.numeric(dellyDelDf$rp_size)
-        dellyDelDf <- dellyDelDf[dellyDelDf$rp_size>=regSizeLowerCutoff &
-                             dellyDelDf$rp_size<=regSizeUpperCutoff &
-                             dellyDelDf$rp_support>=readsSupport, ]
-        dellyDelDf$name <- NULL
-        dellyDelDf <- dellyDelDf[, 1:6]
-    } else {
-        dellyDelDf <- NULL
+    dellyCont <- do.call(rbind, dellyFileCont)
+    
+    if (pass) {
+      dellyCont <- dellyCont[dellyCont$pass=="PASS", ]
     }
+    
+    dellyCont$end <- as.numeric(gsub(".+;END=(\\d+);.+", "\\1", dellyCont$info))
+    dellyCont$type <- gsub(".+;SVTYPE=([A-Z]+);.+", "\\1", dellyCont$info)
+    dellyCont$rp_support <- as.numeric(gsub(".+;PE=(\\d+);.+", "\\1", dellyCont$info))
+    dellyCont$map_quality <- as.numeric(gsub(".+;MAPQ=(\\d+).*", "\\1", dellyCont$info))
+    dellyCont$size <- as.numeric(gsub(".+;SVLEN=(\\d+);.+", "\\1", dellyCont$info))
+    
+    dellyCont <- dellyCont[dellyCont$map_quality>=minMappingQuality&
+                          dellyCont$rp_support>=readsSupport&
+                            dellyCont$size>=regSizeLowerCutoff&
+                            dellyCont$size<=regSizeUpperCutoff, ]
+    
+    dellyCont <- dellyCont[, c("chr", "start", "end", "size", "rp_support",
+                               "map_quality", "type")]
+
+    dellyDelDf <- dellyCont[dellyCont$type=="DEL", ]
 
     ## filtering and merging deletions
     if (!is.null(dellyDelDf)) {
         dellyDelIrange <- GRanges(seqnames=dellyDelDf$chr, 
-                              ranges=IRanges(start=dellyDelDf$rp_left, 
-                                             end=dellyDelDf$rp_right))
+                              ranges=IRanges(start=dellyDelDf$start, 
+                                             end=dellyDelDf$end))
         dellyDelIrangeRes <- findOverlaps(dellyDelIrange, reduce(dellyDelIrange))
         dellyDelDf$clu <- subjectHits(dellyDelIrangeRes)
         dellyDelDfFilMer <- ddply(dellyDelDf, ("clu"), dellyCluster)
-        dellyDelDfFilMer <- dellyDelDfFilMer[, 2:5]
+        dellyDelDfFilMer <- dellyDelDfFilMer[, c("chr", "start", "end", "size")]
         names(dellyDelDfFilMer) <- c("chromosome", "pos1", "pos2", "size")
     } else {
         dellyDelDfFilMer <- NULL
     }
 
     ## reading duplications
-    dellyDupRpList <- list.files(dataDir, full.names=T, pattern=".+\\Dup$")
-    dellyDupSrList <- list.files(dataDir, full.names=T, pattern=".+\\Dup\\.br$")
-
-    dellyDupRp <- lapply(dellyDupRpList, function(x) {
-        dellyData <- try(read.table(x, fill=T, as.is=T, comment.char="+"), silent=T)
-        if (is.data.frame(dellyData)) {
-            dellyData <- dellyData[grepl("Duplication", dellyData$V7), ]
-            dellyData <- dellyData[, 1:7]
-            names(dellyData) <- c("chr", "rp_left", "rp_right", "rp_size", 
-                              "rp_support", "rp_map_quality", "id")
-            return(dellyData)
-        } else {
-            return(NULL)
-        }
-    })
-
-    dellyDupSr <- lapply(dellyDupSrList, function(x) {
-        dellyData <- try(read.table(x, fill=T, col.names=paste("V", 1:13, sep=""), 
-                                as.is=T), silent=T)
-        if (is.data.frame(dellyData)) {
-            dellyData <- dellyData[grepl("Duplication", dellyData$V7), ]
-            dellyData <- dellyData[, 1:7]
-            names(dellyData) <- c("chr", "sr_left", "sr_right", "sr_size", 
-                              "sr_support", "sr_aln_quality", "id")
-            return(dellyData)
-        } else {
-            return(NULL)
-        }
-    })
-
-    dellyDupRpDf <- do.call(rbind, dellyDupRp)
-    dellyDupSrDf <- do.call(rbind, dellyDupSr)
-
-    dellyDupDf <- NULL
-    if (is.data.frame(dellyDupRpDf) & is.data.frame(dellyDupSrDf)) {
-        dellyDupDf <- merge(dellyDupRpDf, dellyDupSrDf, by=c("id", "chr"), all=T)
-        dellyDupDf$rp_left <- as.numeric(dellyDupDf$rp_left)
-        dellyDupDf$rp_right <- as.numeric(dellyDupDf$rp_right)
-        dellyDupDf$rp_size <- as.numeric(dellyDupDf$rp_size)
-        dellyDupDf <- dellyDupDf[dellyDupDf$rp_size>=regSizeLowerCutoff &
-                             dellyDupDf$rp_size<=regSizeUpperCutoff &
-                             dellyDupDf$rp_support>=readsSupport, ]
-        dellyDupDf$name <- NULL
-        dellyDupSrNna <- which(!(is.na(dellyDupDf$sr_size)))
-        if (length(dellyDupSrNna)>0) {
-            dellyDupDf[dellyDupSrNna, ]$rp_left <- dellyDupDf[dellyDupSrNna, ]$sr_left
-            dellyDupDf[dellyDupSrNna, ]$rp_right <- dellyDupDf[dellyDupSrNna, ]$sr_right
-            dellyDupDf[dellyDupSrNna, ]$rp_size <- dellyDupDf[dellyDupSrNna, ]$sr_size
-        }
-        dellyDupDf <- dellyDupDf[, 1:6]
-        dellyDupDf$rp_left <- as.numeric(dellyDupDf$rp_left)
-        dellyDupDf$rp_right <- as.numeric(dellyDupDf$rp_right)
-        dellyDupDf$rp_size <- as.numeric(dellyDupDf$rp_size)
-    } else if (is.data.frame(dellyDupRpDf)) {
-        dellyDupDf <- dellyDupRpDf
-        dellyDupDf$rp_left <- as.numeric(dellyDupDf$rp_left)
-        dellyDupDf$rp_right <- as.numeric(dellyDupDf$rp_right)
-        dellyDupDf$rp_size <- as.numeric(dellyDupDf$rp_size)
-        dellyDupDf <- dellyDupDf[dellyDupDf$rp_size>=regSizeLowerCutoff &
-                             dellyDupDf$rp_size<=regSizeUpperCutoff &
-                             dellyDupDf$rp_support>=readsSupport, ]
-        dellyDupDf$name <- NULL
-        dellyDupDf <- dellyDupDf[, 1:6]
-    } else if (is.data.frame(dellyDupSrDf)) {
-        dellyDupDf <- dellyDupSrDf
-        names(dellyDupDf)[2:5] <- c("rp_left","rp_right","rp_size","rp_support")
-        dellyDupDf$rp_left <- as.numeric(dellyDupDf$rp_left)
-        dellyDupDf$rp_right <- as.numeric(dellyDupDf$rp_right)
-        dellyDupDf$rp_size <- as.numeric(dellyDupDf$rp_size)
-        dellyDupDf <- dellyDupDf[dellyDupDf$rp_size>=regSizeLowerCutoff &
-                             dellyDupDf$rp_size<=regSizeUpperCutoff &
-                             dellyDupDf$rp_support>=readsSupport, ]
-        dellyDupDf$name <- NULL
-        dellyDupDf <- dellyDupDf[, 1:6]
-    } else {
-        dellyDupDf <- NULL
-    }
+    dellyDupDf <- dellyCont[dellyCont$type=="DUP", ]
 
     ## filtering and merging duplications
     if (!is.null(dellyDupDf)) {
         dellyDupIrange <- GRanges(seqnames=dellyDupDf$chr, 
-                              ranges=IRanges(start=dellyDupDf$rp_left, 
-                                             end=dellyDupDf$rp_right))
+                              ranges=IRanges(start=dellyDupDf$start, 
+                                             end=dellyDupDf$end))
         dellyDupIrangeRes <- findOverlaps(dellyDupIrange, reduce(dellyDupIrange))
         dellyDupDf$clu <- subjectHits(dellyDupIrangeRes)
         dellyDupDfFilMer <- ddply(dellyDupDf, ("clu"), dellyCluster)
-        dellyDupDfFilMer <- dellyDupDfFilMer[, 2:5]
+        dellyDupDfFilMer <- dellyDupDfFilMer[, c("chr", "start", "end", "size")]
         names(dellyDupDfFilMer) <- c("chromosome", "pos1", "pos2", "size")
     } else {
         dellyDupDfFilMer <- NULL
     }
 
     ## reading inversions
-    dellyInvRp.list <- list.files(dataDir, full.names=T, pattern=".+\\.inv$")
-    dellyInvSr.list <- list.files(dataDir, full.names=T, 
-                                  pattern=".+\\.inv\\.br$")
-
-    dellyInvRp <- lapply(dellyInvRp.list, function(x) {
-        dellyData <- try(read.table(x, fill=T, as.is=T, comment.char="+"), silent=T)
-        if (is.data.frame(dellyData)) {
-            dellyData <- dellyData[grepl("Inversion", dellyData$V7), ]
-            dellyData <- dellyData[, 1:7]
-            names(dellyData) <- c("chr", "rp_left", "rp_right", 
-                              "rp_size", "rp_support", "o_rp_map_qul", "id")
-            return(dellyData)
-        } else {
-            return(NULL)
-        }
-    })
-    dellyInvRpDf <- do.call(rbind, dellyInvRp)
-    
-    dellyInvSr <- lapply(dellyInvSr.list, function(x) {
-        dellyData <- try(read.table(x, fill=T, col.names=paste("V", 1:13, sep=""), 
-                                as.is=T), silent=T)
-        if (is.data.frame(dellyData)) { 
-            dellyData <- dellyData[grepl("Inversion", dellyData$V7), ]
-            dellyData <- dellyData[, 1:7]
-            names(dellyData) <- c("chr", "sr_left", "sr_right", "sr_size", 
-                              "sr_supp", "sr_aln_qul", "id")
-            return(dellyData)
-        } else {
-            return(NULL)
-        }
-    })
-    dellyInvSrDf <- do.call(rbind, dellyInvSr)
-
-    dellyInvDf <- NULL
-    if (is.data.frame(dellyInvRpDf) & is.data.frame(dellyInvSrDf)) {
-        dellyInvDf <- merge(dellyInvRpDf, dellyInvSrDf, by=c("id", "chr"), all=T)
-        dellyInvDf$rp_left <- as.numeric(dellyInvDf$rp_left)
-        dellyInvDf$rp_right <- as.numeric(dellyInvDf$rp_right)
-        dellyInvDf$rp_size <- as.numeric(dellyInvDf$rp_size)
-        dellyInvDf <- dellyInvDf[dellyInvDf$rp_size>=regSizeLowerCutoff &
-                             dellyInvDf$rp_size<=regSizeUpperCutoff &
-                             dellyInvDf$rp_support>=readsSupport, ]
-        dellyInvDf$name <- NULL
-        dellyInvSrNna <- which(!(is.na(dellyInvDf$sr_size)))
-        if (length(dellyInvSrNna)>0) {
-            dellyInvDf[dellyInvSrNna, ]$rp_left <- dellyInvDf[dellyInvSrNna, ]$sr_left
-            dellyInvDf[dellyInvSrNna, ]$rp_right <- dellyInvDf[dellyInvSrNna, ]$sr_right
-            dellyInvDf[dellyInvSrNna, ]$rp_size <- dellyInvDf[dellyInvSrNna, ]$sr_size
-        }
-        dellyInvDf <- dellyInvDf[, 1:6]
-        dellyInvDf$rp_left <- as.numeric(dellyInvDf$rp_left)
-        dellyInvDf$rp_right <- as.numeric(dellyInvDf$rp_right)
-        dellyInvDf$rp_size <- as.numeric(dellyInvDf$rp_size)
-    } else if (is.data.frame(dellyInvRpDf)) {
-        dellyInvDf <- dellyInvRpDf
-        dellyInvDf$rp_left <- as.numeric(dellyInvDf$rp_left)
-        dellyInvDf$rp_right <- as.numeric(dellyInvDf$rp_right)
-        dellyInvDf$rp_size <- as.numeric(dellyInvDf$rp_size)
-        dellyInvDf <- dellyInvDf[dellyInvDf$rp_size>=regSizeLowerCutoff &
-                             dellyInvDf$rp_size<=regSizeUpperCutoff &
-                             dellyInvDf$rp_support>=readsSupport, ]
-        dellyInvDf$name <- NULL
-        dellyInvDf <- dellyInvDf[, 1:6]
-    } else if (is.data.frame(dellyInvSrDf)) {
-        dellyInvDf <- dellyInvSrDf
-        names(dellyInvDf)[2:5] <- c("rp_left","rp_right","rp_size","rp_support")
-        dellyInvDf$rp_left <- as.numeric(dellyInvDf$rp_left)
-        dellyInvDf$rp_right <- as.numeric(dellyInvDf$rp_right)
-        dellyInvDf$rp_size <- as.numeric(dellyInvDf$rp_size)
-        dellyInvDf <- dellyInvDf[dellyInvDf$rp_size>=regSizeLowerCutoff &
-                             dellyInvDf$rp_size<=regSizeUpperCutoff &
-                             dellyInvDf$rp_support>=readsSupport, ]
-        dellyInvDf$name <- NULL
-        dellyInvDf <- dellyInvDf[, 1:6]
-    } else {
-        dellyInvDf <- NULL
-    }
+    dellyInvDf <- dellyCont[dellyCont$type=="INV", ]
     
     ## filtering and merging inversions
     if (!is.null(dellyInvDf)) {
         dellyInvIrange <- GRanges(seqnames=dellyInvDf$chr, 
-                              ranges=IRanges(start=dellyInvDf$rp_left, 
-                                             end=dellyInvDf$rp_right))
+                              ranges=IRanges(start=dellyInvDf$start, 
+                                             end=dellyInvDf$end))
         dellyInvIrangeRes <- findOverlaps(dellyInvIrange, reduce(dellyInvIrange))
         dellyInvDf$clu <- subjectHits(dellyInvIrangeRes)
         dellyInvDfFilMer <- ddply(dellyInvDf, ("clu"), dellyCluster)
-        dellyInvDfFilMer <- dellyInvDfFilMer[, 2:5]
+        dellyInvDfFilMer <- dellyInvDfFilMer[, c("chr", "start", "end", "size")]
         names(dellyInvDfFilMer) <- c("chromosome", "pos1", "pos2", "size")
     } else {
         dellyInvDfFilMer <- NULL
@@ -313,5 +118,5 @@ readDelly <- function(dataDir=".", regSizeLowerCutoff=100,
                 inv=dellyInvDfFilMer)
     attributes(retuRes) <- c(attributes(retuRes), list(method=method))
     
-    return(retuRes);
+    return(retuRes)
 }
