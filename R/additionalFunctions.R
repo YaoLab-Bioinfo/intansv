a5059d5913054cdc16e21158fabdcd209e383b2e
psytky03 <psytky03@users.noreply.github.com>
2017-09-13 20:09:52 +0900

    MergeOLCNVs
    Implemnent function MergeOLCNVs to merge overlapping CNVs with similar sizes to fix the issues in ReadDelly and ReadBreakDancer.

---
+++ R/additionalFunctions.R
@@ -0,0 +1,71 @@
+# A function to calculate the overlaping percentage between query and subject in GRanges or IRange format
+tellOLPercantage <- function(query, subject, ...) {
+  require(GenomicRanges) 
+  require(IRanges)
+  if(!((class(query)=="GRanges" & class(subject)=="GRanges") | (class(query)=="IRanges" & class(subject)=="IRanges"))) {
+    stop("Query and subject need to be of same class, either GRanges or IRanges!")
+  }
+  hits <- findOverlaps(query, subject)
+  overlaps <- pintersect(query[queryHits(hits)],subject[subjectHits(hits)])
+  olp_percentage <- width(overlaps)/width(subject[subjectHits(hits)])
+  return(olp_percentage)
+}
+
+
+# MergeOLCNVs: A function to merge overlapping CNVs similar in size
+mergeOLCNVs <- function(df, percentage = 0.6, software="", ...){
+  if(!class(df)=="data.frame"){
+    stop("a dataframe is required as the input")
+  }
+
+  GRSubj <- makeGRangesFromDataFrame(df,keep.extra.columns = T)
+  self_overlap <- countOverlaps(GRSubj, GRSubj , type="any")
+  
+  UniqCNVs    <- GRSubj[self_overlap == 1,] #to be returned
+  NonUniqCNVs <- GRSubj[self_overlap >  1,] 
+  
+  #self_overlap_cycle_2 (so2)
+  so2<- tellOLPercantage(NonUniqCNVs, reduce(NonUniqCNVs))
+  
+  #PassedCNVs_in_cycle_2 (Pass_2)
+  #FailedCNVs_in_cycle_2 (Fail_2)
+  
+  Pass2nd <- NonUniqCNVs[so2 <= percentage, ] #to be returned
+  Fail2nd <- NonUniqCNVs[so2 >  percentage, ] 
+  
+  Fail2nd$clu <- subjectHits(findOverlaps(Fail2nd, reduce(Fail2nd)))
+
+  #Passed_CNVs_in_cycle_3 (pc3)
+  Pass3rd <- GRanges()
+  for (i in unique(Fail2nd$clu)){
+    CNV_in_clu <- Fail2nd[Fail2nd$clu==i,]
+    if(length(CNV_in_clu) == 1){
+      CNV_in_clu$clu <- NULL
+      Pass3rd<- append(Pass3rd, CNV_in_clu)
+    }else{
+      best_supported <- CNV_in_clu[which.max(CNV_in_clu$rp_support), ]
+      best_supported$clu <- NULL
+      Pass3rd <- append(Pass3rd, best_supported)
+    }
+  }
+  
+  FinalSet <- GRanges()
+  FinalSet <- append(FinalSet,UniqCNVs)
+  FinalSet <- append(FinalSet,Pass2nd)
+  FinalSet <- append(FinalSet,Pass3rd)
+  if(software == "DELLY"){
+    info_str <- paste0("PE=", FinalSet$pe_support, ";", "SR=", FinalSet$sr_support)
+    }else if(software == "BreakDancer"){
+    info_str <- paste0("score=", FinalSet$score, ";", "PE=", FinalSet$rp_support)
+    }
+
+  reformt_df <- data.frame(chromosome = seqnames(FinalSet), 
+                           pos1 = start(FinalSet), 
+                           pos2 = end(FinalSet),
+                           size = FinalSet$size,
+                           info = info_str,
+                           stringsAsFactors=FALSE
+                           )
+  reformt_df$chromosome = as.character(reformt_df$chromosome)
+  return(reformt_df)
+}
--- R/readDelly.R
+++ R/readDelly.R
@@ -1,32 +1,6 @@
-
-## merging overlapped SVs predicted by DELLY
-dellyCluster <- function(df) 
-{
-    maxReadPairSupp <- max(df$rp_support)
-    dfFil <- df[df$rp_support>=(maxReadPairSupp/2), ]
-    dfFilIrange <- IRanges(start=dfFil$start, end=dfFil$end)
-    outTmp <- findOverlaps(dfFilIrange, reduce(dfFilIrange))
-    dfFil$cludelly <- subjectHits(outTmp)
-    dfFilRes <- ddply(dfFil, ("cludelly"), function(x){
-        if(nrow(x)==1){
-            return(x)
-        } else {
-            LeftMin <- min(x$start)
-            RightMax <- max(x$end)
-            RangeLength <- RightMax-LeftMin
-            x$op <- (x$end - x$start)/RangeLength
-            if(any(x$op<0.8)){
-                return(NULL)
-            } else {
-                return(x[which.max(x$rp_support), ])
-            }
-        }
-    })
-}
-
 ## Reading in the predicted SVs given by DELLY
 readDelly <- function(dataDir=".", regSizeLowerCutoff=100, 
-                      regSizeUpperCutoff=1000000, readsSupport=3,
+                      regSizeUpperCutoff=10000000, readsSupport=3,
                       method="DELLY", pass=TRUE, minMappingQuality=20) 
 {
     ## reading in SV predictions
@@ -66,82 +40,13 @@ readDelly <- function(dataDir=".", regSizeLowerCutoff=100,
                             dellyCont$size>=regSizeLowerCutoff&
                             dellyCont$size<=regSizeUpperCutoff, ]
     
-    dellyCont <- dellyCont[, c("chr", "start", "end", "size", "rp_support",
-                               "pe_support", "sr_support", "map_quality", "type")]
-
-    dellyDelDf <- dellyCont[dellyCont$type=="DEL", ]
-
-    ## filtering and merging deletions
-    if (!is.null(dellyDelDf) && nrow(dellyDelDf)>0) {
-        dellyDelIrange <- GRanges(seqnames=dellyDelDf$chr, 
-                              ranges=IRanges(start=dellyDelDf$start, 
-                                             end=dellyDelDf$end))
-        dellyDelIrangeRes <- findOverlaps(dellyDelIrange, reduce(dellyDelIrange))
-        dellyDelDf$clu <- subjectHits(dellyDelIrangeRes)
-        dellyDelDfFilMer <- ddply(dellyDelDf, ("clu"), dellyCluster)
-        dellyDelDfFilMer <- dellyDelDfFilMer[, c("chr", "start", "end", "size", "rp_support",
-                                           "pe_support", "sr_support")]
-        names(dellyDelDfFilMer) <- c("chromosome", "pos1", "pos2", "size", "readsSupport",
-                                           "pe_support", "sr_support")
-	dellyDelDfFilMer$readsSupport <- NULL
-	dellyDelDfFilMer$info <- paste0("PE=", dellyDelDfFilMer$pe_support, ";",
-				       "SR=", dellyDelDfFilMer$sr_support)
-	dellyDelDfFilMer$pe_support <- NULL
-	dellyDelDfFilMer$sr_support <- NULL
-    } else {
-        dellyDelDfFilMer <- NULL
-    }
-
-    ## reading duplications
-    dellyDupDf <- dellyCont[dellyCont$type=="DUP", ]
-
-    ## filtering and merging duplications
-    if (!is.null(dellyDupDf) && nrow(dellyDupDf)>0) {
-        dellyDupIrange <- GRanges(seqnames=dellyDupDf$chr, 
-                              ranges=IRanges(start=dellyDupDf$start, 
-                                             end=dellyDupDf$end))
-        dellyDupIrangeRes <- findOverlaps(dellyDupIrange, reduce(dellyDupIrange))
-        dellyDupDf$clu <- subjectHits(dellyDupIrangeRes)
-        dellyDupDfFilMer <- ddply(dellyDupDf, ("clu"), dellyCluster)
-        dellyDupDfFilMer <- dellyDupDfFilMer[, c("chr", "start", "end", "size", "rp_support",
-                                           "pe_support", "sr_support")]
-        names(dellyDupDfFilMer) <- c("chromosome", "pos1", "pos2", "size", "readsSupport",
-                                           "pe_support", "sr_support")
-	dellyDupDfFilMer$readsSupport <- NULL
-	dellyDupDfFilMer$info <- paste0("PE=", dellyDupDfFilMer$pe_support, ";",
-				       "SR=", dellyDupDfFilMer$sr_support)
-	dellyDupDfFilMer$pe_support <- NULL
-	dellyDupDfFilMer$sr_support <- NULL
-    } else {
-        dellyDupDfFilMer <- NULL
-    }
-
-    ## reading inversions
-    dellyInvDf <- dellyCont[dellyCont$type=="INV", ]
     
-    ## filtering and merging inversions
-    if (!is.null(dellyInvDf) && nrow(dellyInvDf)>0) {
-        dellyInvIrange <- GRanges(seqnames=dellyInvDf$chr, 
-                              ranges=IRanges(start=dellyInvDf$start, 
-                                             end=dellyInvDf$end))
-        dellyInvIrangeRes <- findOverlaps(dellyInvIrange, reduce(dellyInvIrange))
-        dellyInvDf$clu <- subjectHits(dellyInvIrangeRes)
-        dellyInvDfFilMer <- ddply(dellyInvDf, ("clu"), dellyCluster)
-        dellyInvDfFilMer <- dellyInvDfFilMer[, c("chr", "start", "end", "size", "rp_support",
-                                           "pe_support", "sr_support")]
-        names(dellyInvDfFilMer) <- c("chromosome", "pos1", "pos2", "size", "readsSupport",
-                                           "pe_support", "sr_support")
-	dellyInvDfFilMer$readsSupport <- NULL
-	dellyInvDfFilMer$info <- paste0("PE=", dellyInvDfFilMer$pe_support, ";",
-				       "SR=", dellyInvDfFilMer$sr_support)
-	dellyInvDfFilMer$pe_support <- NULL
-	dellyInvDfFilMer$sr_support <- NULL
-    } else {
-        dellyInvDfFilMer <- NULL
-    }
+    dellyDelDf <- mergeOLCNVs(dellyCont[dellyCont$type =="DEL", ], software= method)
+    dellyDupDf <- mergeOLCNVs(dellyCont[dellyCont$type =="DUP", ], software= method)
+    dellyINVDf <- mergeOLCNVs(dellyCont[dellyCont$type =="INV", ], software= method)
 
-    retuRes <- list(del=dellyDelDfFilMer, dup=dellyDupDfFilMer, 
-                inv=dellyInvDfFilMer)
+
+    retuRes <- list(del=dellyDelDf, dup= dellyDupDf, inv=dellyINVDf)
     attributes(retuRes) <- c(attributes(retuRes), list(method=method))
     
     return(retuRes)
